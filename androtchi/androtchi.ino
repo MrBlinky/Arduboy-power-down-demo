2fe6c16fc9756b907ad8cbc2ee02725fa122e1d3
Mr.Blinky <mstr.blinky@gmail.com>
2018-01-20 21:59:57 +0100

    Add files via upload

---
+++ androtchi/androtchi.ino
@@ -0,0 +1,58 @@
+#include <Arduboy2.h>
+#include <Sprites.h>
+#include "bitmap.h"
+#include "src/powerdown.h"
+
+Arduboy2 arduboy;
+
+int16_t androtchi_x = (WIDTH - androtchi_width) / 2;
+int16_t androtchi_y = (HEIGHT - androtchi_width) /2;
+uint8_t androtchi_frame;         
+               
+void setup() 
+{
+  autoPowerDownInit();
+  arduboy.begin();
+  arduboy.initRandomSeed();
+  arduboy.setFrameRate(4); //framerate < 4 not supported
+}
+
+void loop() 
+{
+  if (powerDownTimeout == 0) setAutoPowerDownTimeout(10); // power down after 10 secs if no buttons are pressed
+  if (powerUpTimeout == 0) setAutoPowerUpTimeout(10);     //force power up after 10 seconds in power down mode. add // at the beginning of this line to disable this behaviour.
+
+  if (arduboy.buttonsState()) setAutoPowerDownTimeout(10); //reset power down time on any button press
+  if (arduboy.pressed(B_BUTTON)) powerDown();              // instand power down when B button is pressed
+
+  if (!arduboy.nextFrame()) return; else delay(375); //additional delay to lower framerate to ~2 fps
+
+  //display  days and time after powering on
+  arduboy.clear();
+  arduboy.print(powertime.days);
+  arduboy.setCursor(WIDTH - 48,0);
+  if (powertime.hours < 10) arduboy.print(0);
+  arduboy.print(powertime.hours);
+  arduboy.print(":");
+  if (powertime.minutes < 10) arduboy.print(0);
+  arduboy.print(powertime.minutes);
+  arduboy.print(":");
+  if (powertime.seconds < 10) arduboy.print(0);
+  arduboy.print(powertime.seconds);
+
+  //draw androtchi tamagotchi robot
+  androtchi_frame = arduboy.frameCount & 1;
+  Sprites::draw(androtchi_x, androtchi_y, androtchi_sprite_plus_mask, androtchi_frame, NULL, 0, SPRITE_PLUS_MASK);
+  if (random() & 1)
+    {
+      androtchi_x += 8;
+      if (androtchi_x > (WIDTH - androtchi_width)) androtchi_x = WIDTH - androtchi_width;
+    }
+    else
+    {
+      androtchi_x-= 8;
+      if (androtchi_x <0) androtchi_x= 0;
+    }
+    
+  arduboy.display();
+}
---
+++ androtchi/src/powerdown.h
@@ -0,0 +1,108 @@
+#ifndef POWERDOWN_H
+#define POWERDOWN_H
+
+#include <Arduino.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <Arduboy2.h>
+
+#ifdef AB_DEVKIT
+  #error "Arduboy DevKit is not supported (No wake up button)"
+#endif
+
+#define PD_INTERVAL_1SEC WDTO_1S
+#define PD_INTERVAL_4SEC WDTO_4S
+
+enum {
+  ps_up,
+  ps_down
+};
+
+struct powertime_t {
+  uint8_t seconds;
+  uint8_t minutes;
+  uint8_t hours;
+  uint16_t days;
+  }; 
+
+#define wdt_interrupt_enable(interval)                                 \
+  asm volatile (                                                       \
+    "in   __tmp_reg__,  __SREG__  \n\t"                                \
+    "cli                          \n\t"                                \
+    "wdr                          \n\t"                                \
+    "sts  %[WDTREG],  %[WDCE_WDE] \n\t"                                \
+    "sts   %[WDTREG],  %[WDVALUE]  \n\t"                                \
+    "out  __SREG__, __tmp_reg__   \n\t"                                \
+    :                                                                  \
+    : [WDTREG]   "M" (_SFR_MEM_ADDR(WDTCSR)),                          \
+      [WDCE_WDE] "r" ((uint8_t)(_BV(_WD_CHANGE_BIT) | _BV(WDE))),      \
+      [WDVALUE]  "r" ((uint8_t)((interval & 0x08 ? (1<<WDP3) : 0x00) | \
+                     (1<<WDIF) | (1<<WDIE) | (interval & 0x07)))       \
+    )
+                         
+/*******************************************************************************
+ * INT6_enable 
+ * Enable INT6 interrupt on low level. (attachInterrupt)
+ ******************************************************************************/
+
+#define INT6_enable() \
+  EICRB &= ~(ISC61 || ISC60); \
+  EIMSK |= _BV(INT6);
+
+/*******************************************************************************
+ * INT6_disable 
+ * Disables INT6 interrupt (detachInterrupt)
+ ******************************************************************************/
+
+#define INT6_disable() \
+  EIMSK &= _BV(INT6);
+
+/*******************************************************************************
+powerDownIO()
+Macro to set all I/O to inputs except the OLED reset pin which is kept as a low
+output. Only a pullup is kept on the A button for a stable powerup button.
+*******************************************************************************/
+ 
+#define powerDownIO()        \
+  DDRB  = 0;                 \
+  PORTB = 0;                 \
+  DDRC  = 0;                 \
+  PORTC = 0;                 \
+  DDRD  = _BV(RST_BIT);      \
+  PORTD = 0;                 \
+  DDRE  = 0;                 \
+  PORTE = _BV(A_BUTTON_BIT); \
+  DDRF  = 0;                 \
+  PORTF = 0;                 
+
+/*******************************************************************************
+activatePowerDown()
+Macro to activate power down with BOD disabled
+*******************************************************************************/
+  
+#define activatePowerDown()            \
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); \
+  cli();                               \
+  sleep_enable();                      \
+  sei();                               \
+  sleep_cpu();                         \
+  sleep_disable();                     \
+  sei();                               \
+  
+/******************************************************************************/
+
+void autoPowerDownInit(uint8_t timeout = PD_INTERVAL_1SEC);
+void setAutoPowerDownTimeout(uint16_t timeout);
+void setAutoPowerUpTimeout(uint16_t timeout);
+void powerDown();
+
+extern volatile uint8_t  powerState;
+extern volatile uint16_t powerDownTimeout;
+extern volatile uint16_t powerUpTimeout;
+extern volatile powertime_t powertime;
+
+#endif //POWERDOWN_H
+
+
