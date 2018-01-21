343dc4cbd0faf2715750e007b129ab162e49bccb
Mr.Blinky <mstr.blinky@gmail.com>
2018-01-21 23:53:19 +0100

    auto power down using WDT
    Auto power down using WDT for the power down timer.

---
+++ autopowerdown-wdt/autopowerdown-wdt.ino
@@ -0,0 +1,33 @@
+#include <Arduboy2.h>
+#include <Sprites.h>
+#include "bitmap.h"
+#include "src/autopowerdown.h"
+
+Arduboy2 arduboy;
+
+int16_t vortex_x = (WIDTH - vortex_width) / 2;
+int16_t vortex_y = (HEIGHT - vortex_height) /2;
+uint8_t vortex_frame;         
+               
+void setup() 
+{
+  autoPowerDownReset();
+  arduboy.begin();
+  arduboy.setFrameRate(60);
+}
+
+void loop() 
+{
+  autoPowerDown();
+  if (!arduboy.nextFrame()) return;
+  
+  if (arduboy.buttonsState()) autoPowerDownReset(); //reset power down time on any button press
+
+  arduboy.clear();
+
+  Sprites::draw(vortex_x, vortex_y, vortex_sprite, vortex_frame ++, NULL, 0, SPRITE_MASKED);
+
+  vortex_frame %= 15;
+    
+  arduboy.display();
+}
